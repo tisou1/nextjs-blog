2d8830961aab2e4ff4b88475a5d00acd6426d4fa
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-09-16 10:45:10 +0800

    chore: preview deployment

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 
-const name = '谢霆锋'
+const name = '谢霆锋123'
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
