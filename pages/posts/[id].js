35197fb542af08f1a4e697268809a18ff4f3a35d
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-09-30 23:03:46 +0800

    chore: use juejin's md theme

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -12,7 +12,7 @@ export default function Post({ postData }) {
         <title>{postData.title}</title>
       </Head>
 
-      <article>
+      <article className='markdown-body'>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date}/>
